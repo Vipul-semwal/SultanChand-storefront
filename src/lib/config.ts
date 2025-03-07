29511a75ba5e4a3928c2a839ea088a94fe217578
AshuYadav2004 <ashutoshyadav81787@gmail.com>
2025-03-07 22:03:50 +0530

    some env not loading so hardcoding it

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,12 +1,12 @@
 import Medusa from "@medusajs/js-sdk"
 
 // Defaults to standard port for Medusa server
-let MEDUSA_BACKEND_URL = "http://localhost:9000"
+let MEDUSA_BACKEND_URL = "https://admin.sultanchandandsons.com"
 
-if (process.env.MEDUSA_BACKEND_URL) {
-  MEDUSA_BACKEND_URL = process.env.MEDUSA_BACKEND_URL
-  console.log("hellosir",process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY)
-}
+// if (process.env.MEDUSA_BACKEND_URL) {
+//   MEDUSA_BACKEND_URL = process.env.MEDUSA_BACKEND_URL
+//   console.log("hellosir",process.env.MEDUSA_BACKEND_URL)
+// }
 
 export const sdk = new Medusa({
   baseUrl: MEDUSA_BACKEND_URL,
